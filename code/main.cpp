53151dc0115bb06425fd4ae62526c149f3335f1a
cfranc11 <cfranc11@eafit.edu.co>
2016-04-19 07:13:46 -0500

    Inicio del proyecto en C++, se establecen los primeros métodos, la principal función por hacer es pasar los flotantes a binarios.

---
+++ code/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+using namespace System;
+
+//Método de prueba
+int suma(int, int){
+
+}
+
+
+//Método que suma 2 matrices
+float add(){
+
+}
+
+//Método que resta 2 matrices
+float sub(){
+
+}
+
+//Método que multiplica 2 matrices
+float mult(){
+
+}
+
+//Método que divide 2 matrices
+float div(){
+
+}
+
+int main(array < System :: String ^> args){
+	Console::WriteLine("Inicio del programa");
+
+
+	return 0;
+}
